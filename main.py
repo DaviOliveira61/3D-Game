9c740b9740df2c0c7264746db13be3190259a4cf
Davi Oliveira <davigbso@hotmail.com>
2023-12-31 10:07:33 -0300

    Scenery texture completed. Camera controlled by mouse.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # Ready to start
+import pygame
 import pygame as pg
 import sys
 from settings import *
@@ -11,7 +12,9 @@ from object_renderer import *
 class Game:
     def __init__(self):
         pg.init()
+        pg.mouse.set_visible(False)
         self.screen = pg.display.set_mode(RES)
+        # self.screen = pg.display.set_mode((0, 0), pygame.FULLSCREEN) #FULLSCREEN
         self.clock = pg.time.Clock()
         self.delta_time = 1
         self.new_game()
@@ -31,7 +34,7 @@ class Game:
         print(f'{self.clock.get_fps():.1f}')
 
     def draw(self):
-        self.screen.fill('black')
+        # self.screen.fill('black')
         self.object_renderer.draw()
         # self.map.draw()
         # self.player.draw()
